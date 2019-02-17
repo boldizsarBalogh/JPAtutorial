5f89cc32c9c87743b7d49f85cabe3985b3576cb3
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-02-17 14:08:29 +0100

    initial commit

---
+++ src/main/java/com/codecool/firstjpaproject/FirstJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.firstjpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJpaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJpaProjectApplication.class, args);
+    }
+
+}
